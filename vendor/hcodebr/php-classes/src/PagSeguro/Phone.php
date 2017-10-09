9de109163b95dcb2fb2473e645446a2f1aacc0a8
João Rangel <joaohcrangel@gmail.com>
2017-10-09 18:03:13 -0300

    Classe Phone

--- vendor/hcodebr/php-classes/src/PagSeguro/Phone.php
+++ vendor/hcodebr/php-classes/src/PagSeguro/Phone.php
@@ -7,4 +7,44 @@ class Phone {
     private $areaCode;
     private $number;
 
+    public function __construct(int $areaCode, int $number)
+    {
+
+        if (!$areaCode || $areaCode < 11 || $areaCode > 99)
+        {
+
+            throw new Exception("Informe o DDD do telefone.");
+
+        }
+
+        if (!$number || strlen($number) < 8 || strlen($number) > 9)
+        {
+
+            throw new Exception("Informe o número do telefone");
+
+        }
+
+        $this->areaCode = $areaCode;
+        $this->number = $number;
+
+    }
+
+    public function getDOMElement():DOMElement
+    {
+
+        $dom = new DOMDocument();
+
+        $phone = $dom->createElement("phone");
+        $phone = $dom->appendChild($phone);
+
+        $areaCode = $dom->createElement("areaCode", $this->areaCode);
+        $areaCode = $phone->appendChild($areaCode);
+
+        $number = $dom->createElement("number", $this->number);
+        $number = $phone->appendChild($number);
+
+        return $phone;
+
+    }
+
 }
